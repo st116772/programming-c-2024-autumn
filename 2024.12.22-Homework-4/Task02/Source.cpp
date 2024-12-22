276ce2920a6214248e4fb616685e17a5cfba4fa7
st116772 <alpetarxumm@gmail.com>
2024-12-22 22:18:02 +0300

    Uploading Task 2 and fixing Task 1

---
+++ 2024.12.22-Homework-4/Task02/Source.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+int main(int argc, char* argv[])
+{
+	int n = 0;
+	int i = 0;
+	int a[1001] = { 0 };
+
+	scanf_s("%d", &n);
+
+	for (i = 1; i <= n; ++i)
+	{
+		scanf_s("%d", &a[i]);
+	}
+
+	int l = 0;
+	int r = 0;
+
+	scanf_s("%d", &l);
+	scanf_s("%d", &r);
+
+	int k = l;
+
+	for (i = l + 1; i <= r; ++i)
+	{
+		if (a[i] > a[k])
+		{
+			k = i;
+		}
+	}
+
+	printf("%d %d", a[k], k);
+
+	return EXIT_SUCCESS;
+}
